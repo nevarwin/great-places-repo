af65d4285f49a679812ba46be6934d26e797e180
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-03-17 22:01:08 +0800

    added a text field and raised button icon

---
+++ great_places_app/lib/screens/add_place_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  const AddPlaceScreen({Key? key}) : super(key: key);
+  static const routeName = '/add-place';
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add a New Place',
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Title',
+                      ),
+                      controller: _textController,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
+            elevation: 0,
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add,
+            ),
+            label: const Text(
+              'Add Place',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
