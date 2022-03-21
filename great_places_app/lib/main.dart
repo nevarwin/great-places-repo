269374525a8988bf1344acbd915a7cb6e474ec3a
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-03-21 21:21:33 +0800

    added routes

--- great_places_app/lib/main.dart
+++ great_places_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,13 +18,15 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => GreatPlaces(),
       child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.amber,
-        ),
-        home: const PlacesListScreen(),
-      ),
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: const PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => const AddPlaceScreen(),
+          }),
     );
   }
 }
