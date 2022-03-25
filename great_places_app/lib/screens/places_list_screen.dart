9448155d7aefa90baa3e52010d94f0b78379f538
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-03-25 21:56:29 +0800

    refactor Consumer with FutureBuilder

--- great_places_app/lib/screens/places_list_screen.dart
+++ great_places_app/lib/screens/places_list_screen.dart
@@ -25,26 +25,40 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: const Center(
-          child: Text('Got no places yet, start adding some!'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
-            ? ch!
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  onTap: () {},
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      greatPlaces.items[i].image,
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(
+          context,
+          listen: false,
+        ).fetchAndSetPlaces(),
+        builder: (
+          ctx,
+          snapshot,
+        ) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<GreatPlaces>(
+                    child: const Center(
+                      child: Text('Got no places yet, start adding some!'),
                     ),
+                    builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
+                        ? ch!
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (ctx, i) => ListTile(
+                              onTap: () {},
+                              leading: CircleAvatar(
+                                backgroundImage: FileImage(
+                                  greatPlaces.items[i].image,
+                                ),
+                              ),
+                              title: Text(
+                                greatPlaces.items[i].title,
+                              ),
+                            ),
+                          ),
                   ),
-                  title: Text(
-                    greatPlaces.items[i].title,
-                  ),
-                ),
-              ),
       ),
     );
   }
