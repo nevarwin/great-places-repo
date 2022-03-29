dadb41d892351826fa57f41e16aa69c05e36397d
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-03-29 21:32:34 +0800

    change selectedLocation into LatLng

--- great_places_app/lib/widgets/location_input.dart
+++ great_places_app/lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../helpers/location_helpers.dart';
 import '../screens/map_screen.dart';
@@ -28,7 +29,7 @@ class _LocationInputState extends State<LocationInput> {
     }
 
     Future<void> _selectOnMap() async {
-      final selectedLocation = await Navigator.of(context).push(
+      final LatLng selectedLocation = await Navigator.of(context).push(
         MaterialPageRoute(
           fullscreenDialog: true,
           builder: (ctx) => MapScreen(
