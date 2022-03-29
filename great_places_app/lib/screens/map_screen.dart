8ac88f9e4ac392f6ef24b80c0073a7011e653ef2
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-03-29 21:32:13 +0800

    added _selectLocation method

--- great_places_app/lib/screens/map_screen.dart
+++ great_places_app/lib/screens/map_screen.dart
@@ -20,13 +20,44 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Set<Marker> mySet = {};
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Map'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedLocation);
+                    },
+              icon: const Icon(
+                Icons.check,
+              ),
+            ),
+        ],
       ),
       body: GoogleMap(
+        markers: _pickedLocation == null
+            ? {}
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  position: _pickedLocation,
+                ),
+              },
+        onTap: widget.isSelecting ? _selectLocation : null,
         initialCameraPosition: CameraPosition(
           zoom: 16,
           target: LatLng(
