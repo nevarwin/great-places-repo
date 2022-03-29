3028d7b33a894988b140fa3b82ea974c96e2ace1
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-03-29 20:46:58 +0800

    turn address to nullable

--- great_places_app/lib/models/place.dart
+++ great_places_app/lib/models/place.dart
@@ -5,12 +5,12 @@ import 'dart:io';
 class PlaceLocation {
   final double latitude;
   final double longitude;
-  final String address;
+  final String? address;
 
-  PlaceLocation({
+  const PlaceLocation({
     required this.latitude,
     required this.longitude,
-    required this.address,
+    this.address,
   });
 }
 
